fe13cb0c874f759201eb076ef26a2ca3eb31b682
Manuel <manuel.baden@hotmail.de>
2023-02-22 20:33:44 +0100

    Fix logic

--- src/useChatInfo.js
+++ src/useChatInfo.js
@@ -4,18 +4,18 @@ import useTwitchChat from "./useTwitchChat";
 
 const useChatInfo = () => {
     const channel = useQuery();
-    const [state, setState] = useState({ "names": {} });
+    const [state, setState] = useState({ "names": {}, "first": undefined });
 
     const onMessage = useCallback((channel, tags, message, self) => {
         const displayName = tags['display-name'];
         console.log(`${displayName}: ${message}`);
         setState((s) => {
-            const c = { "names": { ...s, ...s["names"] } };
+            const c = { ...s };
             c["names"][displayName] = (c["names"][displayName] || 0) + 1;
             if (!c["first"]) {
                 if (c["names"][displayName] === 1) {
                     c["first"] = displayName;
-                    setTimeout(() => setState(st => ({ "names": { ...st, ...st["names"] }, "first": undefined })), 5000);
+                    setTimeout(() => setState(st => ({ ...st, "first": undefined })), 5000);
                 }
             }
             return c;
