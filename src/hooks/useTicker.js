aaa15415c592a0bf820bf2977607060aa5d5321a
Manuel <manuel.baden@hotmail.de>
2023-02-25 16:29:34 +0100

    Initialize full array

--- src/hooks/useTicker.js
+++ src/hooks/useTicker.js
@@ -2,18 +2,16 @@ import { useCallback, useEffect, useState } from "react";
 import useFirstChatter from "./useFirstChatter";
 import usePostillion from "./usePostillion";
 
-const maxMessages = 10;
+const maxMessages = 6;
+const preFilledArray = Array.apply(null, Array(maxMessages)).map(i => i);
 
 const useTicker = () => {
-    const [state, setMessageList] = useState({ messageList: [], index: 0 });
+    const [state, setMessageList] = useState({ messageList: preFilledArray, index: 0 });
     const postillionTicker = usePostillion();
 
     const pushMessage = useCallback((message) => {
         console.log("pushMessage", message);
         setMessageList(oldState => {
-            if (oldState.messageList.length < maxMessages) {
-                return { ...oldState, messageList: oldState.messageList.concat([message]) }
-            }
             const newMessageList = [...oldState.messageList];
             newMessageList[oldState.index] = message;
             return { messageList: newMessageList, index: (oldState.index + 1) % maxMessages };
